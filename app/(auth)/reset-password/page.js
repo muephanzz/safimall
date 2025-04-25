6ba50b89c51a7d5ec2951edb168da4e0c50224e6
ephantus <muephanzz@gmail.com>
2025-04-25 18:37:39 +0300

    auth

--- app/(auth)/reset-password/page.js
+++ app/(auth)/reset-password/page.js
@@ -89,5 +89,5 @@ const ResetPassword = () => {
 
 export default ResetPassword;
 
-//git config --global http.proxy http://proxy_host:proxy_port
-//git config --global https.proxy https://proxy_host:proxy_port
+//git config --global http.proxy http://192.168.6.42:7071
+//git config --global https.proxy https://192.168.6.42:7071
