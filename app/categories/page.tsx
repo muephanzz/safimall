3acc710a3683601022a0f0ee138faa8faec217c1
Ephantus <muephanzz@gmail.com>
2025-05-17 15:29:19 +0300

    done for today

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -1,9 +1,28 @@
+import ProductHeader from "@/components/ProductHeader";
 import MobileMenu from "@/components/TabletNav";
+import { useEffect, useState } from "react";
 
 export default function CategoriesPage() {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+      // Detect mobile device based on user agent
+      const checkMobile = () => {
+        if (typeof navigator !== "undefined") {
+          setIsMobile(/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent));
+        }
+      };
+      checkMobile();
+    }, []);
+
   return (
-    <div>
-      <MobileMenu />
-    </div>
+    <>
+      <div>
+        <ProductHeader isMobile={isMobile} />
+      </div>
+      <div>
+        <MobileMenu />
+      </div>
+    </>
   );
 }
--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,5 +1,28 @@
 import ChatPage from "@/components/ChatModel";
+import ProductHeader from "@/components/ProductHeader";
+import { useEffect, useState } from "react";
 
 export default function ContactRoute() {
-  return <ChatPage />;
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+      // Detect mobile device based on user agent
+      const checkMobile = () => {
+        if (typeof navigator !== "undefined") {
+          setIsMobile(/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent));
+        }
+      };
+      checkMobile();
+    }, []);
+
+  return (
+    <>
+      <div>
+        <ProductHeader isMobile={isMobile} />
+      </div>
+      <div>
+        <ChatPage />
+      </div>
+    </>
+  );
 }
